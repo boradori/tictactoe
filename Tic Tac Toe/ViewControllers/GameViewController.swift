d571e399312d3d10e35269efdabdd79a3085955d
Youngsun Paik <youngsunpaik@gmail.com>
2018-08-14 06:38:14 -0700

    game logic finished

--- Tic Tac Toe/ViewControllers/GameViewController.swift
+++ Tic Tac Toe/ViewControllers/GameViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class GameViewController: UIViewController {
   var board = Board(grids: [0, 0, 0, 0, 0, 0, 0, 0, 0])
+  private var gameIsActive = true
   
   var playerOne = PlayerOne(name: "Player 1", symbol: UIImage(assetIdentifier: .cross), wins: 0)
   var playerTwo = PlayerTwo(name: "Player 2", symbol: UIImage(assetIdentifier: .nought), wins: 0)
@@ -70,10 +71,7 @@ class GameViewController: UIViewController {
   
   
   @IBAction func openMenu(_ sender: UIBarButtonItem) {
-    menuLeadingConstraint.constant = menuLeadingConstraint.constant == 0 ? -140 : 0
-    UIView.animate(withDuration: 0.2) {
-      self.view.layoutIfNeeded()
-    }
+    slideSidebar()
   }
   
   @objc private func playerOneCardTapped() {
@@ -88,7 +86,7 @@ class GameViewController: UIViewController {
 
   
   @IBAction func placeSymbol(_ sender: UIButton) {
-    if board.grids[sender.tag - 1] == 0 {
+    if board.grids[sender.tag - 1] == 0 && gameIsActive == true {
       board.grids[sender.tag - 1] = activePlayer
       
       if activePlayer == 1 {
@@ -99,6 +97,56 @@ class GameViewController: UIViewController {
         sender.setImage(UIImage(assetIdentifier: .nought), for: UIControlState())
         activePlayer = 1
       }
+    }
+    
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
+    for combination in winningCombinations {
+      if board.grids[combination[0]] != 0 && board.grids[combination[0]] == board.grids[combination[1]] && board.grids[combination[1]] == board.grids[combination[2]] {
+        gameIsActive = false
+        
+        if board.grids[combination[0]] == 1 {
+          print("cross")
+        } else {
+          print("nought")
+        }
+      }
+    }
+    
+    if gameIsActive == true {
+      gameIsActive = false
+      
+      for grid in board.grids {
+        if grid == 0 {
+          gameIsActive = true
+          break
+        }
+      }
+      
+      if gameIsActive == false {
+        print("draw")
+      }
+    }
+    
+  }
+  
+  @IBAction func startNewGame(_ sender: UIButton) {
+    board.grids = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    gameIsActive = true
+    activePlayer = 1
+    
+    for i in 1...9 {
+      let button = view.viewWithTag(i) as! UIButton
+      button.setImage(UIImage(assetIdentifier: .emptyGrid), for: UIControlState())
+    }
+    
+    slideSidebar()
+  }
+  
+  private func slideSidebar() {
+    menuLeadingConstraint.constant = menuLeadingConstraint.constant == 0 ? -140 : 0
+    UIView.animate(withDuration: 0.2) {
+      self.view.layoutIfNeeded()
     }
   }
   
